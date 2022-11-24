d9a8154f019a1e899246bc68a6004d2f5ee6d2fb
Ahmed <MAhmed61@its.jnj.com>
2022-11-24 17:33:35 +0530

    added meow meow function \

--- test.py
+++ test.py
@@ -4,4 +4,18 @@ def default():
 def main():
     default()
 if __name__ == '__main__':
+    main()import sys
+def default():
+    print("Hello")
+
+
+def cat():
+    print(" Meow Meow")
+
+def main():
+    if sys.argv[1]=='cat':
+        cat()
+    else:
+        default()
+if __name__ == '__main__':
     main()
