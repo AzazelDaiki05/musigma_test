d184ad39a7902e916a9d31ce138b81db2567475b
Shrikant Richhariya <shrikant.javarnd@gmail.com>
2022-11-24 18:13:12 +0530

    Shrikant_HTML_AddedBowBow

--- test.py
+++ test.py
@@ -4,4 +4,24 @@ def default():
 def main():
     default()
 if __name__ == '__main__':
+    main()import sys
+def default():
+    print("Hello")
+
+
+def cat():
+    print(" Meow Meow")
+
+
+def dog():
+    print(" bhaw bhaw")
+
+def main():
+    if sys.argv[1]=='cat':
+        cat()
+    elif sys.argv[1]=='dog':
+        dog()
+    else:
+        default()
+if __name__ == '__main__':
     main()
